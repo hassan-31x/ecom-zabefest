ba8fb12c9d1f21196c530dea520e9437f7befead
Hassan <muhammadhassanchannel786@gmail.com>
2024-05-07 02:53:24 +0500

    login replaced mui with shadcn

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -3,13 +3,19 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import axios from 'axios';
 
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 interface FormData {
     email: string;
@@ -19,27 +25,29 @@ const LoginForm = () => {
   const [submitting, setSubmitting] = useState(false);
   const validationSchema = Yup.object().shape({
     email: Yup.string()
-      .email('Invalid email address')
-      .required('Email is required'),
+    .required('Email is required')
+      .email('Invalid email address'),
     password: Yup.string()
       .required('Password is required'),
   });
 
-  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+  const form = useForm({
     resolver: yupResolver(validationSchema)
   });
 
-  const onSubmit = async (data:FormData) => {
+
+  const onSubmit = async (data: FormData) => {
     setSubmitting(true);
     try {
       const response = await axios.post('http://localhost:5000/api/v1/auth/login', data);
       if (response.status === 200) {
         console.log(response.data);
         alert('Login successful!');
-        reset();
+        form.reset();
       } else {
         throw new Error('Login failed');
       }
+      console.log(data)
     } catch (error) {
       console.error(error);
       alert('Login failed');
@@ -50,54 +58,40 @@ const LoginForm = () => {
 
 
   return (
-    <Container maxWidth="sm">
-      <Box
-        sx={{
-          marginTop: 5,
-          padding: 4,
-          boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-          borderRadius: 4,
-          backgroundColor: '#ffffff',
-        }}
-      >
-        <Typography variant="h4" align="center" gutterBottom sx={{ fontWeight: 'bold' }}>
-          Login
-        </Typography>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-            label="Email"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            placeholder="Enter your email address"
-            {...register('email')}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-          />
-          <TextField
-            label="Password"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            type="password"
-            placeholder="Enter your password"
-            {...register('password')}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-            disabled={submitting}
-            sx={{ marginTop: 2 }}
-          >
-            {submitting ? 'Logging in...' : 'Login'}
-          </Button>
-        </form>
-      </Box>
-    </Container>
+    <div>
+
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    </div>
   );
 };
 
