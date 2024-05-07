cf1dd6950f6513bdb616920f1ba6b5715122728a
Mujeeb ur rehman <mujeebfx160@gmail.com>
2024-05-07 20:17:04 +0500

    Feat : Product Qrscanner

---
+++ client/app/checkout/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import QrScanner from "@/components/ui/qrscanner";
+
+const page = () => {
+  return (
+    <div>
+        <QrScanner/>
+    </div>
+  )
+}
+
+export default page
