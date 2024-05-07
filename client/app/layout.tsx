d588a2a757e38b0bdf9d23aafa3be26ad3afb6b1
Hassan <muhammadhassanchannel786@gmail.com>
2024-05-07 17:35:33 +0500

    useAuth fixed

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "@/providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <ToastContainer />
-      </body>
-
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+          <ToastContainer />
+        </body>
+      </AuthProvider>
     </html>
   );
 }
--- client/components/user-button.tsx
+++ client/components/user-button.tsx
@@ -1,8 +1,54 @@
 "use client"
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu";
+  import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useAuth } from "@/providers/AuthProvider";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 const UserButton = () => {
+    const { user, setUser } = useAuth();
+  const router = useRouter();
+
+
   return (
-    <div>Hello</div>
+    <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+      <Button
+        variant="secondary"
+        size="icon"
+        className="rounded-full bg-bg text-text border-text border-2 hover:bg-bg/50"
+      >
+        <Avatar>
+          <AvatarImage
+            src={user?.avatar?.url || "/images/user.webp"}
+            alt={user?.username || "user"}
+            className="object-cover"
+          />
+          <AvatarFallback>...</AvatarFallback>
+        </Avatar>
+        <span className="sr-only">Toggle user menu</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end" className="bg-bg">
+      <DropdownMenuLabel className="text-text">My Account</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem className="text-text hover:!bg-neutral-800 hover:!text-text">
+        <Link href="/settings">Settings</Link>
+      </DropdownMenuItem>
+      <DropdownMenuItem className="text-text hover:!bg-neutral-800 hover:!text-text">
+        {/* <LogoutBtn /> */}
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
   )
 }
 
