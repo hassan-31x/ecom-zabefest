ecd641add43ffcfe0a37e530eac906bacab1db8b
Hassan <muhammadhassanchannel786@gmail.com>
2024-05-08 10:28:10 +0500

    added suspense

--- client/app/(authenticated)/success/page.tsx
+++ client/app/(authenticated)/success/page.tsx
@@ -5,6 +5,12 @@ import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 
 import Success from "./_components/successCoomponent";
+import { Suspense } from "react";
+
+function SearchBarFallback() {
+  return <>loading...</>
+}
+
 const SuccessPage = () => {
   return (
     <div className="relative">
@@ -12,7 +18,9 @@ const SuccessPage = () => {
         <ArrowLeft size={25} className="" />
       </Link>
       <div>
+      <Suspense fallback={<SearchBarFallback />}>
         <Success />
+      </Suspense>
       </div>
     </div>
   )
